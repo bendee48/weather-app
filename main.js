8695037ae1712acd10b4b28cd03dcafbbf90483c
Ben Daniels <benbanj@hotmail.com>
2023-05-24 21:11:13 +0100

    Adding function to call api

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("yes I work");
+const apiKey = '441a55bf31ca493eadd235239231704';
+
+function getWeather(query='london') {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+getWeather('california')
