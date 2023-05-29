185fca7a8b8d3f44e404235244ff763d7b4b67d1
Ben Daniels <benbanj@hotmail.com>
2023-05-29 21:34:55 +0100

    Processing API data
    - Adding function to process data reurned from api

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 const apiKey = '441a55bf31ca493eadd235239231704';
 
+// Call api {returns Promise}
 function getWeather(query='london') {
   const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}`;
-  fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data));
+  const res = fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                  console.log(data)
+                  return data;
+                })
+                .catch(err => console.log(err))
+
+  return Promise.resolve(res);
 }
 
-getWeather('california')
+// Process json returned from API for usable info
+function processData(data) {
+  let tempC = data.current.temp_c;
+  let tempF = data.current.temp_f;
+  let condition = data.current.condition.text;
+  let icon = data.current.condition.icon;
+  let country = data.location.country;
+  let area = data.location.name;
+
+  return { tempC, tempF, condition, icon, country, area }
+}
+
+
+getWeather('manchester').then(data => console.log(processData(data)))
