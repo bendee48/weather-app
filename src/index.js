37a333d893c662cdc476c26ae9ddc94675eea2e7
Ben Daniels <bendee@bens-mbp.home>
2023-08-03 17:33:07 +0100

    Moving js code from main to index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('yes I work')
+const apiKey = '441a55bf31ca493eadd235239231704';
+
+// Call api {returns Promise}
+function getWeather(query='london') {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}`;
+  const res = fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                  console.log('from fetch', data)
+                  return data;
+                })
+                .catch(err => console.log('eRRor', err))
+
+  return res;
+}
+
+// Process json returned from API for usable info
+function processData(data) {
+  let tempC = data.current.temp_c;
+  let tempF = data.current.temp_f;
+  let condition = data.current.condition.text;
+  let icon = data.current.condition.icon;
+  let country = data.location.country;
+  let area = data.location.name;
+
+  return { tempC, tempF, condition, icon, country, area }
+}
+
+
+getWeather('la').then(data => console.log(data)).catch(err => console.log('cant process data'))
